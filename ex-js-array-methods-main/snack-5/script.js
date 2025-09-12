31369b05a12e4bc9b817c5dc0c694acb0bdcc125
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-12 14:20:35 +0200

    risolvi es 5

--- ex-js-array-methods-main/snack-5/script.js
+++ ex-js-array-methods-main/snack-5/script.js
@@ -2,3 +2,8 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+
+//uso il filtro per tirar fuori solo i numeri pari
+const evenNumbs = nums.filter(numb => numb % 2 === 0);
+console.log(evenNumbs);
