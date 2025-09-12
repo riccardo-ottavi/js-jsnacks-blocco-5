5285b6d4e3d95f7c0884e47b1338f23cbe26365a
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-12 14:02:41 +0200

    risolvi es. 3

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -3,7 +3,7 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-//ciclo l'array e via via mando in console la proprty richiesta
+//ciclo l'array e via via mando in console i valori
 for(let i = 0; i < names.length; i++){
     console.log(names[i]);
 }
--- ex-js-array-methods-main/snack-3/script.js
+++ ex-js-array-methods-main/snack-3/script.js
@@ -2,3 +2,7 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+//uso map per creare un array di numeri incrementati partendo da numbers
+const incremented = numbers.map((number => number +1));
+console.log(incremented);
