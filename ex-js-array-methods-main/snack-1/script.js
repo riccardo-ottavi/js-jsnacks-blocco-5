babc1f09a940cb57bb8145944272cc8f39726140
RiccardoOttavi <ottavi.ps@libero.it>
2025-09-12 13:57:48 +0200

    risolvi es 1

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,9 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+//ciclo l'array e via via mando in console la proprty richiesta
+for(let i = 0; i < names.length; i++){
+    console.log(names[i]);
+}
+
